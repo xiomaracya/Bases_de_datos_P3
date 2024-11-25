b3e55c989248a1f4b463bdcce2e4476785c5debd
xiomaracya <lizbethyxiomara@gmail.com>
2024-11-25 01:35:56 +0100

    mi version

--- tester.c
+++ tester.c
@@ -25,10 +25,10 @@ void checkCreateIndex(const char * indexName);
 void checkCreateTable(const char * tableName);
 
 void checkPrint(const char * indexName);
-void createTestIndexFile(const char * indexName);
+/*void createTestIndexFile(const char * indexName);
 void checkFindKey(const char * indexName);
 void checkAddIndexEntry(const char * indexName);
-
+*/
 
 int main() {
     const char *tableName = "myDataBase.dat";
@@ -44,10 +44,10 @@ int main() {
      * implement it
      * checkReplaceExtensionByIdx(tableName, indexName); */
     checkCreateIndex(indexName);
-    /*checkCreateTable(tableName);
+    checkCreateTable(tableName);
     createTestFiles(tableName, indexName);
     checkPrint(indexName);
-    checkFindKey(indexName);
+    /*checkFindKey(indexName);
     checkAddIndexEntry(indexName);*/
 #ifdef NEVERDEFINED
 #endif
--- utils.c
+++ utils.c
@@ -1,10 +1,14 @@
 #include "utils.h"
+#include <stdlib.h>
 #include <string.h>
+#define BufferLength 100
 
 int no_deleted_registers = NO_DELETED_REGISTERS;
 
 void replaceExtensionByIdx(const char *fileName, char * indexName) {
-    indexName = strtok((char*)fileName, ".");
+    char name[BufferLength] = "\0";
+    strcpy(name, fileName);
+    indexName = strtok(name, ".");
     strcat(indexName, ".idx");
     return;
 }
@@ -12,15 +16,19 @@ void replaceExtensionByIdx(const char *fileName, char * indexName) {
 
 bool createTable(const char * tableName) {
     FILE *f = NULL;
+    int element=-1;
     char *indexName = "\0";
 
-    f=fopen(tableName, "r");
+    f=fopen(tableName, "rb");
     if(f == NULL){
-        f=fopen(tableName, "w");
+        f=fopen(tableName, "wb");
         if(f == NULL){
             return false;
         }
-        fprintf(f, "-1");
+        if(fwrite(&element, sizeof(int), 1, f)!=1){
+            fclose(f);
+            return false;
+        }
     }
 
     replaceExtensionByIdx(tableName, indexName);
@@ -56,13 +64,67 @@ bool createIndex(const char *indexName) {
     fclose(f);
     return true;
 }
-void printnode(size_t _level, size_t level,
-               FILE * indexFileHandler, int node_id, char side) {
+void printnode(size_t _level, size_t level, FILE * indexFileHandler, int node_id, char side) {
+    int h1= 0, h2=0 , p= 0, datos= 0, i;
+    char pk[5] = {0};
+
+    if (_level >= level) {
+        printf("Profundidad máxima alcanzada");
+        return;
+    }
+
+    if(fread(pk, sizeof(char), sizeof(pk)-1, indexFileHandler)!=sizeof(pk)-1){
+        printf("ERROR1");
+        return;
+    }
+    if(fread(&h1, sizeof(int), 1, indexFileHandler)!=1){
+        printf("ERROR2");
+        return;
+    }
+    if(fread(&h2, sizeof(int), 1, indexFileHandler)!=1){
+        printf("ERROR3");
+        return;
+    }
+    if(fread(&p, sizeof(int), 1, indexFileHandler)!=1){
+        printf("ERROR4");
+        return;
+    }
+    if(fread(&datos, sizeof(int), 1, indexFileHandler)!=1){
+        printf("ERROR5");
+        return;
+    }
+
+    for (i = 0; i < (int)_level; i++) {
+        printf("\t"); 
+    }
+    printf("%c %s (%d) : %d\n", side, pk, node_id, datos);
+
+    fseek(indexFileHandler, h1, SEEK_SET);
+    printnode(_level + 1, level, indexFileHandler, node_id*2+1, 'l');
+    fseek(indexFileHandler, h2, SEEK_SET);
+    printnode(_level + 1, level, indexFileHandler, node_id*2+2, 'r');
     return;
 }
 
 void printTree(size_t level, const char * indexName)
 {
+    FILE *f = NULL;
+    int loc_root = -1;
+    f = fopen(indexName, "rb");
+    if (f == NULL){
+        printf("Error al abrir el fichero");
+        return;
+    }
+    if (fread(&loc_root, sizeof(int), 1, f) != 1 || loc_root < 0){
+        fclose(f);
+        printf("El fichero esta vacío");
+        return;
+    }
+    fseek(f, loc_root, SEEK_SET);
+    printnode(0, level, f, 0,' ');
+    if (f){
+        fclose(f);
+    }
     return;
 }
 
